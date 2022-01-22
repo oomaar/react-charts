626506a3a29e9fc838a20d8a6c9ba21eecfaa899
Omar <omaar_5@hotmail.com>
2022-01-22 23:53:21 +0200

    render all Routes in App.js Private and public

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login } from "./Pages";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
+import { Landing, Login, Signup } from "./Pages";
+import { PrivateRoute } from "./helpers/routes";
+import { Layout } from "./Global";
+import React from "react";
 
 export const App = () => {
   return (
     <div>
       <Router basename="/">
         <Routes>
-          <Route exact path={`/`} element={<Login />} />
+          <Route exact path="/" element={<Login title="Login" subTitle="Enter your email and password" />} />
+          <Route exact path="/signup" element={<Signup title="Signup" subTitle="Create account" />} />
+          <Route element={<PrivateRoute />}>
+            <Route element={<Layout><Outlet /></Layout>}>
+              <Route exact path={`/landing`} element={<Landing />} />
+            </Route>
+          </Route>
         </Routes>
       </Router>
     </div>
