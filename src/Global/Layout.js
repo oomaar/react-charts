2868d7c40963b31ddef617760d3ffe6179e1dd9c
oomaar <omaar_5@hotmail.com>
2022-01-23 12:37:45 +0200

    setup Sidebar

--- src/Global/Layout.js
+++ src/Global/Layout.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle } from '.';
+import { Sidebar } from '../Components';
 
 export const Layout = ({ children }) => {
+    const [toggleSidebar, setToggleSidebar] = useState(true);
+    const handleToggleSidebar = () => setToggleSidebar(state => !state);
+
     return (
         <div>
             <GlobalStyle />
+            <Sidebar toggleSidebar={toggleSidebar} />
             {children}
         </div>
     );
