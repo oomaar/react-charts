2e45979ce82ec93ae00e7a6459f1b05a4deadb14
Omar <omaar_5@hotmail.com>
2022-01-30 00:53:03 +0200

    setup Landing Page

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,10 +1,164 @@
-import React from 'react';
-import { Container } from "./styledLanding";
+import React, { useEffect, useState } from 'react';
+import ReactHighcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import axios from 'axios';
+import {
+    Container,
+    TopContainer,
+    ChartsTitle,
+    ChartsContainer,
+    ChartCard,
+} from "./styledLanding";
+import { Table } from '../../Components';
 
 export const Landing = () => {
+    const [appPerformance, setAppPerformance] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get(`https://my-json-server.typicode.com/WorexEG/json-server/appPerformance`);
+            setAppPerformance(res.data);
+        };
+
+        fetchData();
+    }, []);
+
+    let appPerformancePie = {};
+
+    const appPerformanceCharts = appPerformance.map((el, index) => {
+        appPerformancePie = {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: el.applicationName
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    size: 150,
+                    zIndex: 1,
+                    colors: ['#7fb7e8', '#E60965'],
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: [{
+                    name: el.applicationName,
+                    y: el.successRate,
+                }, {
+                    name: 'Failure Rate',
+                    y: el.failerRate
+                }]
+            }]
+        }
+
+        return (
+            <ChartCard key={index}>
+                <HighchartsReact highcharts={ReactHighcharts} options={appPerformancePie} />
+            </ChartCard>
+        );
+    });
+
+    const allAppsColumns = [
+        {
+            Header: "Application Name",
+            accessor: "applicationName"
+        },
+        {
+            Header: "Failure Percentage",
+            accessor: "failerRate"
+        }
+    ];
+
+    const devicesColumns = [
+        {
+            Header: 'Application Name',
+            accessor: 'applicationName'
+        },
+        {
+            Header: 'device UserName',
+            accessor: 'deviceUserName'
+        },
+        {
+            Header: 'Time',
+            accessor: 'Time'
+        },
+        {
+            Header: 'Date',
+            accessor: 'Date'
+        }
+    ];
+
+    const devicesTableData = [
+        {
+            applicationName: 'Zendesk',
+            deviceUserName: 'Hassan',
+            Time: '5pm',
+            Date: '1/23/2022'
+        },
+        {
+            applicationName: 'Oracle NetSuite',
+            deviceUserName: 'Ramzy',
+            Time: '2am',
+            Date: '1/22/2022'
+        },
+        {
+            applicationName: 'Microsoft Dynamics',
+            deviceUserName: 'Omar',
+            Time: '4pm',
+            Date: '1/20/2022'
+        },
+        {
+            applicationName: 'Salesforce',
+            deviceUserName: 'Ramzy',
+            Time: '11pm',
+            Date: '1/19/2022'
+        },
+        {
+            applicationName: 'Zendesk',
+            deviceUserName: 'Omar',
+            Time: '7pm',
+            Date: '1/5/2022'
+        }
+    ]
+
     return (
         <Container>
-            <h1>Landing Page</h1>
+            <TopContainer>
+                <ChartsTitle>Application Performance</ChartsTitle>
+                <ChartsContainer>
+                    {appPerformanceCharts}
+                </ChartsContainer>
+            </TopContainer>
+            <Table
+                cols={allAppsColumns}
+                title="All Applications"
+                url="https://my-json-server.typicode.com/WorexEG/json-server/appPerformance"
+            />
+
+            <Table
+                cols={devicesColumns}
+                title="Devices"
+                data={devicesTableData}
+            />
         </Container>
     );
 };
--- src/Pages/Landing/styledLanding.js
+++ src/Pages/Landing/styledLanding.js
@@ -1,3 +1,54 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 2rem;
+
+  @media screen and (max-width: 1024px) {
+    padding: 1rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    padding: 0;
+  }
+`;
+
+export const TopContainer = styled.div`
+  background-color: #fff;
+  border-radius: 1rem;
+  padding: 1rem 0;
+`;
+
+export const ChartsTitle = styled.p`
+  border-bottom: 1px solid #f5f5f5;
+  padding: 0 1rem 1rem;
+`;
+
+export const ChartsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+`;
+
+export const ChartCard = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 700px;
+  margin: 1rem auto;
+  border-radius: 1rem;
+  padding: 1rem;
+  box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.1);
+  z-index: 1;
+
+  @media screen and (max-width: 1024px) {
+    width: 600px;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 280px;
+    padding: 0;
+  }
+`;
