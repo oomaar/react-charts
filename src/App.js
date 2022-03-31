4328157928fc7759856fb35ec995d451dd28f821
Omar <omaar_5@hotmail.com>
2022-03-31 19:03:51 +0200

    setup Login Screen

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import {
   Outlet,
 } from "react-router-dom";
 import React from "react";
+import { Login } from "./Screens";
 
 export const App = () => {
   return (
     <div>
       <Router basename="/">
         <Routes>
-          <Route exact path="/" element={<p>Login</p>} />
+          <Route exact path="/" element={<Login />} />
           <Route element={<>Private Route</>}>
             <Route
               element={
---
+++ src/Screens/Login/Login.js
@@ -0,0 +1,5 @@
+import { LoginContainer } from "./styledLogin";
+
+export const Login = () => {
+  return <LoginContainer>Login</LoginContainer>;
+};
