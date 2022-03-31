c29bde4e5563203c03326b89ca530dc50a3bf45b
Omar <omaar_5@hotmail.com>
2022-03-31 19:21:24 +0200

    Login All HTML

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -1,5 +1,48 @@
-import { LoginContainer } from "./styledLogin";
+import {
+  LoginContainer,
+  LoginSubContainer,
+  LoginImage,
+  LoginFormContainer,
+  LoginIcon,
+  LoginTitle,
+  LoginForm,
+  LoginFormInputContainer,
+  LoginFormInputLabel,
+  LoginFormInput,
+  LoginFormButton,
+  LoginCopy,
+} from "./styledLogin";
 
 export const Login = () => {
-  return <LoginContainer>Login</LoginContainer>;
+  return (
+    <LoginContainer>
+      <LoginSubContainer>
+        <LoginImage>
+          <h1>
+            <span>Fly</span> Insights
+          </h1>
+          <img src="/images/login-photo.png" alt="Dashboard png" />
+          <LoginTitle>Welcome to Fly Insights !</LoginTitle>
+        </LoginImage>
+        <LoginFormContainer>
+          <LoginIcon>
+            <img src="/images/user.png" alt="User Avatar" />
+          </LoginIcon>
+          <LoginTitle>Sign in to your account</LoginTitle>
+          <LoginForm>
+            <LoginFormInputContainer>
+              <LoginFormInputLabel>Username</LoginFormInputLabel>
+              <LoginFormInput />
+            </LoginFormInputContainer>
+            <LoginFormInputContainer>
+              <LoginFormInputLabel>Password</LoginFormInputLabel>
+              <LoginFormInput />
+            </LoginFormInputContainer>
+            <LoginFormButton>Sign in</LoginFormButton>
+            <LoginCopy>Copyright © 2022 Fly Insights.</LoginCopy>
+          </LoginForm>
+        </LoginFormContainer>
+      </LoginSubContainer>
+    </LoginContainer>
+  );
 };
--- src/Screens/Login/styledLogin.js
+++ src/Screens/Login/styledLogin.js
@@ -1,3 +1,52 @@
 import styled from "styled-components";
 
-export const LoginContainer = styled.div``;
+export const LoginContainer = styled.div`
+  background: url("/images/login-bg.jpg");
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginSubContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginImage = styled.div`
+  background: #fff;
+  padding: 1rem;
+
+  img {
+  }
+
+  h1 {
+    span {
+    }
+  }
+`;
+
+export const LoginFormContainer = styled.div`
+  background: #4650be;
+`;
+
+export const LoginIcon = styled.div`
+  img {
+  }
+`;
+
+export const LoginTitle = styled.p``;
+
+export const LoginForm = styled.form``;
+
+export const LoginFormInputContainer = styled.div``;
+
+export const LoginFormInputLabel = styled.label``;
+
+export const LoginFormInput = styled.input``;
+
+export const LoginFormButton = styled.button``;
+
+export const LoginCopy = styled.h4``;
