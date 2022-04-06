b457a0f9553e41678aa11d84df561325fd9fec3a
Omar <omaar_5@hotmail.com>
2022-04-06 23:42:00 +0200

    style Navbar input

--- src/Layout/Navbar/Navbar.js
+++ src/Layout/Navbar/Navbar.js
@@ -1,5 +1,36 @@
-import { NavbarNav } from "./styledNavbar";
+import { useState } from "react";
+import {
+  NavbarNav,
+  UserContainer,
+  UserAvatar,
+  UserName,
+  SearchContainer,
+  SearchInputIcon,
+  SearchInput,
+} from "./styledNavbar";
 
 export const Navbar = () => {
-  return <NavbarNav>TODO: Navbar</NavbarNav>;
+  const [openSearchBar, setOpenSearchBar] = useState(false);
+
+  return (
+    <NavbarNav>
+      <SearchContainer openSearchBar={openSearchBar}>
+        <SearchInputIcon onClick={() => setOpenSearchBar((state) => !state)}>
+          <i class="bx bx-search-alt-2" />
+        </SearchInputIcon>
+        <SearchInput
+          openSearchBar={openSearchBar}
+          type="text"
+          required
+          placeholder="Search Pages"
+        />
+      </SearchContainer>
+      <UserContainer>
+        <UserAvatar>
+          <i class="bx bxs-user" />
+          <UserName>Sign In</UserName>
+        </UserAvatar>
+      </UserContainer>
+    </NavbarNav>
+  );
 };
--- src/Layout/Navbar/styledNavbar.js
+++ src/Layout/Navbar/styledNavbar.js
@@ -2,4 +2,47 @@ import styled from "styled-components";
 
 export const NavbarNav = styled.nav`
   border: 1px solid red;
+  position: fixed;
+  inset: 0;
+  bottom: unset;
+  height: 50px;
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+  gap: 0 1rem;
 `;
+
+export const SearchContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid
+    ${({ openSearchBar, theme }) =>
+      openSearchBar ? theme.colors.borderColor : "#fff"};
+  width: ${({ openSearchBar }) => (openSearchBar ? "200px" : "40px")};
+  height: 35px;
+  border-radius: 1rem;
+  padding: 0.5rem;
+  transition: all 0.3s ease-in;
+`;
+
+export const SearchInputIcon = styled.div`
+  font-size: 1.1rem;
+  cursor: pointer;
+`;
+
+export const SearchInput = styled.input`
+  background: transparent;
+  border: 0;
+  outline: 0;
+  font-size: 1rem;
+  padding-left: 0.5rem;
+  color: #fff;
+  width: ${({ openSearchBar }) => (openSearchBar ? "100%" : "0")};
+  transition: all 0.5s ease-in;
+`;
+
+export const UserContainer = styled.div``;
+
+export const UserAvatar = styled.div``;
+
+export const UserName = styled.p``;
