42eb89ce944baa80c30847914711aa5350c8b625
Omar Hassan <omaar_5@hotmail.com>
2022-04-19 23:21:11 +0200

    Update ApplicationsPerfromance.js

--- src/Screens/ApplicationsPerfromance/ApplicationsPerfromance.js
+++ src/Screens/ApplicationsPerfromance/ApplicationsPerfromance.js
@@ -4,9 +4,12 @@ import { PerformanceContainer } from "./styledApplicationsPerfromance";
 export const ApplicationsPerfromance = () => {
   return (
     <PerformanceContainer>
-      {/* <h1>TODO: Top Used Applications (Charts)</h1> */}
       <TopCharts />
       <h1>TODO: Applications Perfromance Table (Table)</h1>
+      <h1>TODO: Applications Perfromance Table (Table): Download Excel & PDF</h1>
+      <h1>TODO: Applications Perfromance Table (Table): List TD(s) with details col.</h1>
+      <h1>TODO: Applications Perfromance Table (Table): pagination & Search</h1>
+  
       <h1>TODO: Applications Perfromance Modal (Tabbed Container)</h1>
       <h1>TODO: Applications Perfromance Devices Modal (Table)</h1>
       <h1>TODO: Applications Perfromance Failures Modal (Table)</h1>
