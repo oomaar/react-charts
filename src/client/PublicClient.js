43d25d62cafa79a392798543ff78070ccb40cdee
Omar <omaar_5@hotmail.com>
2022-04-20 21:05:59 +0200

    delete un-needed files

--- src/client/PublicClient.js
+++
@@ -1,7 +0,0 @@
-import { Outlet } from "react-router-dom";
-import { Login } from "../Screens";
-import { getToken } from "./client-utils/utils";
-
-export const PublicClient = () => {
-  return getToken() ? <Outlet /> : <Login />;
-};
