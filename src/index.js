94aab191c7ac1c53d31312ee016c65191bc5a32d
Omar <omaar_5@hotmail.com>
2022-04-20 21:04:21 +0200

    wrap the app in appProviders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
 import { App } from "./App";
+import { AppProviders } from "./context";
 
 render(
-  <>
+  <AppProviders>
     <App />
-  </>,
+  </AppProviders>,
   document.getElementById("root")
 );
