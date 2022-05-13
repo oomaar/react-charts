9dc0ff66a9a825354a677f834f91f8eece6c454c
oomaar <omaar_5@hotmail.com>
2022-05-13 13:20:36 +0200

    remove warnings

--- src/Screens/ApplicationsPerformance/ApplicationsPerformance.js
+++ src/Screens/ApplicationsPerformance/ApplicationsPerformance.js
@@ -18,6 +18,7 @@ export const ApplicationsPerformance = () => {
       setData(response.data);
     };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
