c572343351a9a034b4d85ebe8c19106cde27b8c3
oomaar <omaar_5@hotmail.com>
2022-05-18 23:01:15 +0200

    [wip] components table design

--- src/Screens/ApplicationsPerformance/ApplicationsPerformance.js
+++ src/Screens/ApplicationsPerformance/ApplicationsPerformance.js
@@ -1,39 +1,89 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { TopCharts } from "../../components";
 import { useAuth } from "../../context/AuthContext";
-import useFetchData from "../../hooks/useFetchData";
-import { PerformanceContainer } from "./styledApplicationsPerformance";
+import {
+  PerformanceContainer,
+  TableContainer,
+  Table,
+  TableHead,
+  TableHeadCell,
+  TableBody,
+  TableRow,
+  TableBodyCell,
+} from "./styledApplicationsPerformance";
+import { TopCharts } from "../../components";
+// import useFetchData from "../../hooks/useFetchData";
 
 export const ApplicationsPerformance = () => {
-  // const [data, setData] = useState([]);
-
-  // const userToken = useAuth().user.token;
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const response = await axios.get(
-  //       `https://flyworex.azurewebsites.net/api/ProcessesPerformance/GetProcessesPerformance`,
-  //       { headers: { Authorization: `Bearer ${userToken}` } }
-  //     );
-  //     setData(response.data);
-  //   };
-  //   fetchData();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
-
-  const { data, error, loading } = useFetchData(
-    "https://flyworex.azurewebsites.net/api/ProcessesPerformance/GetProcessesPerformance"
-  );
+  const [data, setData] = useState([]);
   console.log(
-    "🚀 ~ file: ApplicationsPerformance.js ~ line 12 ~ ApplicationsPerformance ~ data",
+    "🚀 ~ file: ApplicationsPerformance.js ~ line 18 ~ ApplicationsPerformance ~ data",
     data
   );
+
+  const userToken = useAuth().user.token;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        `https://flyworex.azurewebsites.net/api/ProcessesPerformance/GetProcessesPerformance`,
+        { headers: { Authorization: `Bearer ${userToken}` } }
+      );
+      setData(response.data);
+    };
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // const { data, error, loading } = useFetchData(
+  //   "https://flyworex.azurewebsites.net/api/ProcessesPerformance/GetProcessesPerformance"
+  // );
+  // console.log(
+  //   "🚀 ~ file: ApplicationsPerformance.js ~ line 28 ~ ApplicationsPerformance ~ data",
+  //   data
+  // );
+
+  const headings = [
+    "Application Name",
+    "Duration (Minutes)",
+    "Failure Percentage",
+    "Number of Devices",
+    "Details",
+  ];
+
+  const tableHeadeings = headings.map((heading, i) => (
+    <TableHeadCell key={i}>{heading}</TableHeadCell>
+  ));
 
   return (
     <PerformanceContainer>
       <TopCharts data={data.slice(0, 4)} />
-      <h1>TODO: Applications Perfromance Table (Table)</h1>
+      <TableContainer>
+        <Table>
+          <TableHead>{tableHeadeings}</TableHead>
+          <TableBody>
+            {data.map((row, i) => {
+              const ratio = row.failureCount / row.totalCount;
+              const percentage = ratio * 100;
+
+              return (
+                <TableRow key={i}>
+                  <TableBodyCell>{row.processName}</TableBodyCell>
+                  <TableBodyCell>
+                    {(row.totalCount / 3).toFixed(2)}
+                  </TableBodyCell>
+                  <TableBodyCell>{`${percentage.toFixed(2)}%`}</TableBodyCell>
+                  <TableBodyCell>{row.computersCount}</TableBodyCell>
+                  <TableBodyCell>
+                    <i className="bx bxs-file-doc" />
+                  </TableBodyCell>
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      {/* <h1>TODO: Applications Perfromance Table (Table)</h1>
       <h1>
         TODO: Applications Perfromance Table (Table): Download Excel & PDF
       </h1>
@@ -45,7 +95,7 @@ export const ApplicationsPerformance = () => {
 
       <h1>TODO: Applications Perfromance Modal (Tabbed Container)</h1>
       <h1>TODO: Applications Perfromance Devices Modal (Table)</h1>
-      <h1>TODO: Applications Perfromance Failures Modal (Table)</h1>
+      <h1>TODO: Applications Perfromance Failures Modal (Table)</h1> */}
     </PerformanceContainer>
   );
 };
--- src/Screens/ApplicationsPerformance/styledApplicationsPerformance.js
+++ src/Screens/ApplicationsPerformance/styledApplicationsPerformance.js
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
 export const PerformanceContainer = styled.div``;
+
+export const TableContainer = styled.div`
+  border: 1px solid whitesmoke;
+  padding: 1rem;
+  border-radius: 0.875rem;
+  margin: 0 1rem;
+`;
+
+export const Table = styled.table``;
+
+export const TableHead = styled.tr`
+  border-bottom: 1px solid whitesmoke;
+`;
+
+export const TableHeadCell = styled.th``;
+
+export const TableBody = styled.tbody``;
+
+export const TableRow = styled.tr``;
+
+export const TableBodyCell = styled.td``;
