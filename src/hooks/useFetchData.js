c85f6826ad404e97bb1d6024522d6137ec0f5101
oomaar <omaar_5@hotmail.com>
2022-05-20 12:10:00 +0200

    create a custom hook to fetchData

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,31 +1,25 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
 
-async function useFetchData(dataURL) {
+function useFetchData(url) {
   const [data, setData] = useState([]);
-  const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
-
   const userToken = useAuth().user.token;
 
   useEffect(() => {
-    const fetchData = () => {
-      setLoading(true);
-      axios
-        .get(`${dataURL}`, {
-          headers: { Authorization: `Bearer ${userToken}` },
-        })
-        .then((response) => setData(response.data.json()))
-        .then(setData)
-        .catch((error) => setError)
-        .finally(() => setLoading(false));
-    };
+    const fetchData = async () =>
+      await axios
+        .get(`${url}`, { headers: { Authorization: `Bearer ${userToken}` } })
+        .then((res) => {
+          setLoading(true);
+          setData(res.data);
+          setLoading(false);
+        });
     fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataURL]);
+  }, []);
 
-  return { data, error, loading };
+  return { data, loading };
 }
 
 export default useFetchData;
