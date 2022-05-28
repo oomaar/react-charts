740e98fc74629c0b70b7d899a01ab1338515c7b9
oomaar <omaar_5@hotmail.com>
2022-05-28 21:19:55 +0200

    set up SearchUser List

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -1,14 +1,79 @@
+import { useState } from "react";
+import { useAuthedClient } from "../../context/AuthedClientContext";
+import useFetchData from "../../hooks/useFetchData";
 import {
   PageHeaderContainer,
   PageHeaderTitle,
   PageFiltersContainer,
+  UserFilterList,
+  UserFilterItem,
+  UserFilterContainer,
 } from "./styledPageHeader";
 
 export const PageHeader = ({ title }) => {
+  const [userList, setUserList] = useState(false);
+  const [userFilterValue, setUserFilterValue] = useState("All Users");
+  const [searchUser, setSearchUser] = useState("");
+
+  const authedClient = useAuthedClient();
+
+  const fetchedData = useFetchData(authedClient.getAllUsers());
+  const data = fetchedData.data.data;
+
+  const allUsers = data
+    ?.filter((item) => {
+      if (searchUser === "") {
+        return item;
+      } else if (
+        item.userName.toLowerCase().includes(searchUser.toLowerCase())
+      ) {
+        return item;
+      }
+
+      return 0;
+    })
+    .map((user) => (
+      <UserFilterItem
+        key={user.id}
+        onClick={() => {
+          setUserFilterValue(user.userName);
+          setUserList(false);
+        }}
+      >
+        {user.userName}
+      </UserFilterItem>
+    ));
+
   return (
     <PageHeaderContainer>
       <PageHeaderTitle>{title}</PageHeaderTitle>
-      <PageFiltersContainer>Filters Goes Here</PageFiltersContainer>
+      <PageFiltersContainer>
+        <UserFilterContainer>
+          <span onClick={() => setUserList((state) => !state)}>
+            <i className="bx bxs-user" /> {userFilterValue}{" "}
+            <i userList={userList} className="bx bx-chevron-down" />
+          </span>
+          <UserFilterList userList={userList}>
+            <div>
+              <input
+                type="text"
+                onChange={(e) => setSearchUser(e.target.value)}
+                value={searchUser}
+              />
+              <i className="bx bx-x" onClick={() => setSearchUser("")} />
+            </div>
+            <UserFilterItem
+              onClick={() => {
+                setUserFilterValue("All Users");
+                setUserList(false);
+              }}
+            >
+              All Users
+            </UserFilterItem>
+            {allUsers}
+          </UserFilterList>
+        </UserFilterContainer>
+      </PageFiltersContainer>
     </PageHeaderContainer>
   );
 };
--- src/components/PageHeader/styledPageHeader.js
+++ src/components/PageHeader/styledPageHeader.js
@@ -19,6 +19,79 @@ export const PageHeaderTitle = styled.h1`
   color: #fff;
 `;
 
-export const PageFiltersContainer = styled.div``;
+export const PageFiltersContainer = styled.div`
+  position: relative;
+`;
 
-export const UserFilters = styled.div``;
+export const UserFilterContainer = styled.div`
+  background: #090daf;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  cursor: pointer;
+  color: #fff;
+  max-width: 250px;
+
+  span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0 1rem;
+    transition: all 0.3s ease;
+
+    .bx-chevron-down {
+      transform: ${({ userList }) =>
+        userList ? "rotate(180deg)" : "rotate(0deg)"};
+    }
+  }
+`;
+
+export const UserFilterList = styled.ul`
+  display: ${({ userList }) => (userList ? "flex" : "none")};
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  z-index: 1000;
+  background: #090daf;
+  list-style-type: none;
+  padding: 0.5rem 1rem;
+  border-radius: 1rem;
+  width: 100%;
+  height: 300px;
+  overflow-y: scroll;
+  right: 0.1rem;
+
+  div {
+    display: flex;
+    position: relative;
+
+    i {
+      color: #000;
+      font-weight: bold;
+      font-size: 1.2rem;
+      cursor: pointer;
+      position: absolute;
+      right: 0.1rem;
+      top: 10%;
+    }
+  }
+
+  input {
+    border: 1px solid #aaa;
+    outline: 0;
+    width: 100%;
+    font-size: 1rem;
+    border-radius: 1rem;
+    padding-left: 1rem;
+  }
+`;
+
+export const UserFilterItem = styled.li`
+  width: 100%;
+  text-align: center;
+  padding: 0.5rem 0;
+  border-radius: 0.5rem;
+
+  :hover {
+    background: #090d91;
+  }
+`;
