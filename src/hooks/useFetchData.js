f6288ca6bd68d7fc47a162d082f0a04a7012781c
oomaar <omaar_5@hotmail.com>
2022-05-28 19:43:14 +0200

    update useFetchData hook

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,41 +1,18 @@
-import axios from "axios";
-import { DateTime } from "luxon";
 import { useState, useEffect } from "react";
-import { useAuth } from "../context/AuthContext";
 
-function useFetchData(url) {
+function useFetchData(dataMethod) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
-  const userToken = useAuth().user.token;
-
-  const initialStartDateYear = DateTime.now().c.year;
-  const initialStartDateMonth = DateTime.now().c.month;
-  const initialStartDateDay = DateTime.now().c.day;
-  const initialStartData = `${initialStartDateYear}-${initialStartDateMonth}-${initialStartDateDay}`;
-  // console.log(
-  //   "🚀 ~ file: useFetchData.js ~ line 12 ~ useFetchData ~ DateTime.now()",
-  //   DateTime.now().c.year
-  // );
-  const initialEndDate = DateTime.now().toJSDate();
-  const initialEndDateFrom = DateTime.fromJSDate(initialEndDate);
-  // console.log(
-  //   "🚀 ~ file: useFetchData.js ~ line 20 ~ useFetchData ~ initialEndDateFrom",
-  //   initialEndDateFrom
-  // );
 
   useEffect(() => {
     const fetchData = async () =>
-      await axios
-        // .get(`${url}?from=${initialStartData}&to=${initialStartData}`, {
-        .get(`${url}`, {
-          headers: { Authorization: `Bearer ${userToken}` },
-        })
-        .then((res) => {
-          setLoading(true);
-          setData(res.data);
-          setLoading(false);
-        });
+      await dataMethod.then((res) => {
+        setLoading(true);
+        setData(res);
+        setLoading(false);
+      });
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return { data, loading };
