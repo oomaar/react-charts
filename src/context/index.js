4dfa9319341ee76a611024b376fcd6926c61d79c
oomaar <omaar_5@hotmail.com>
2022-05-28 19:59:40 +0200

    use Authed Client Context

--- src/context/index.js
+++ src/context/index.js
@@ -1,7 +1,12 @@
 import { AuthProvider } from "./AuthContext";
+import { AuthedClientProvider } from "./AuthedClientContext";
 
 function AppProviders({ children }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return (
+    <AuthProvider>
+      <AuthedClientProvider>{children}</AuthedClientProvider>
+    </AuthProvider>
+  );
 }
 
 export { AppProviders };
